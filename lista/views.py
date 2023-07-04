7820029292b758af4d461535703235654be5b8a1
Henry <henryairoso@gmail.com>
2023-07-04 19:19:38 -0300

    Ajustes

--- lista/views.py
+++ lista/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from lista.forms import ListaViagemForm
+from lista.models import ListaViagem
 
 def criar_lista(request):
     if request.method == 'POST':
@@ -12,3 +13,19 @@ def criar_lista(request):
     else:
         form = ListaViagemForm()
     return render(request, 'criar_lista.html', {'form': form})
+
+def editar_lista(request, lista_id):
+    lista = ListaViagem.objects.get(id=lista_id)
+    if request.method == 'POST':
+        form = ListaViagemForm(request.POST, instance=lista)
+        if form.is_valid():
+            form.save()
+            return redirect('nome_da_rota')
+    else:
+        form = ListaViagemForm(instance=lista)
+    return render(request, 'editar_lista.html', {'form': form, 'lista': lista})
+
+def deletar_lista(request, lista_id):
+    lista = ListaViagem.objects.get(id=lista_id)
+    lista.delete()
+    return redirect('nome_da_rota')
